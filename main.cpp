d2b015cc709041017dd790626137a08e48bdb76e
Daniel Trnka <daniel.trnka@gmail.com>
2016-09-24 01:26:59 +0200

    removed useless event handlers

--- main.cpp
+++ main.cpp
@@ -21,10 +21,6 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 	printf("key_callback [%d,%d,%d,%d] \n", key, scancode, action, mods);
 }
 
-static void window_focus_callback(GLFWwindow* window, int focused){ printf("window_focus_callback \n"); }
-
-static void window_iconify_callback(GLFWwindow* window, int iconified){ printf("window_iconify_callback \n"); }
-
 static void window_size_callback(GLFWwindow* window, int width, int height){
 	printf("resize %d, %d \n", width, height);
 	glViewport(0, 0, width, height);
@@ -73,15 +69,8 @@ int main(void)
 
 	// Sets the key callback
 	glfwSetKeyCallback(window, key_callback);
-
 	glfwSetCursorPosCallback(window, cursor_callback);
-
 	glfwSetMouseButtonCallback(window, button_callback);
-
-	glfwSetWindowFocusCallback(window, window_focus_callback);
-
-	glfwSetWindowIconifyCallback(window, window_iconify_callback);
-
 	glfwSetWindowSizeCallback(window, window_size_callback);
 
 	GLenum err = glewInit();
