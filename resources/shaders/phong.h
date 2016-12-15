13d3fa377173860606595a93feae1621f8230b27
Daniel Trnka <daniel.trnka@gmail.com>
2016-12-15 14:55:12 +0100

    Enabled attenuation and specular

--- resources/shaders/phong.h
+++ resources/shaders/phong.h
@@ -23,7 +23,7 @@ vec3 applyLight(Light light, vec3 color, vec3 N) {
 	} else {
 		lightVector = normalize(light.position.xyz - position_world);
 		float distanceToLight = length(light.position.xyz - position_world.xyz);
-		//attenuation = 1.0 / (1.0 + light.attenuation * distanceToLight*distanceToLight);
+		attenuation = 1.0 / (1.0 + light.attenuation * distanceToLight*distanceToLight);
 
 		// spotlight
 		if(light.coneAngle > 0.0) {
@@ -39,7 +39,6 @@ vec3 applyLight(Light light, vec3 color, vec3 N) {
 	vec3 camDir = normalize(cameraPos - position_world.xyz);
 	float spec = pow(max(0.0, dot(camDir, reflect(-lightDir, N))), material.shininess);
 	vec3 specular = material.shininessStrength * material.specularColor * light.specularColor * spec;
-	specular = vec3(0);
 
 	return attenuation * (diffuse + specular);
 }
